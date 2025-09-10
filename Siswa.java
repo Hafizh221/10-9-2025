9e67e52f79f933627b14a374b2cea9e31a976b0c
HafizhRPL <hafizh_syarif_34rpl@student.smktelkom-mlg.sch.id>
2025-09-10 07:23:49 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Siswa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Jumlah Siswa:");
+        int NilaiAkhir = input.nextInt();
+        int RataRata;
+        int temp = 0;
+
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nilai Siswa ke-" + i + ":");
+            int NilaiSiswa = input.nextInt();
+            temp += NilaiSiswa;
+        }
+System.out.println("Rata rata nilai siswa adalah: " + (temp / NilaiAkhir));
+
+if (temp >=5) {
+    System.out.println("Maaf, Keterangan ini tidak valid");
+    
+}
+    }
+}
